b9c36797320f01ea8abb8c9428a36cad2b6f1553
Adam Byle <abyle@walterpmoore.com>
2024-08-21 16:35:10 -0500

    Full camera control

--- script.ts
+++ script.ts
@@ -79,18 +79,18 @@ const elOffsetDebug = <HTMLParagraphElement>document.getElementById("offset-debu
 
 let refreshes = 0;
 
-navigator.geolocation.watchPosition(position => {
-    refreshes++;
-    const newCoords = new Coords(position.coords.latitude, position.coords.longitude);
-    elOffsetDebug.innerText = `Offset: ${newCoords.distanceTo(myCoords)}`
-    myCoords = newCoords;
-    elCoordsDebug.innerText = `${myCoords.lat} ${myCoords.lng}`;
-    elRefreshesDebug.innerText = `Refreshes: ${refreshes}`;
-    if (position.coords.speed) {
-        mySpeed = position.coords.speed;
-    }
-    myAccuracy = position.coords.accuracy;
-}, geolocationError, { enableHighAccuracy: true, maximumAge: 1000 });
+// navigator.geolocation.watchPosition(position => {
+//     refreshes++;
+//     const newCoords = new Coords(position.coords.latitude, position.coords.longitude);
+//     elOffsetDebug.innerText = `Offset: ${newCoords.distanceTo(myCoords)}`
+//     myCoords = newCoords;
+//     elCoordsDebug.innerText = `${myCoords.lat} ${myCoords.lng}`;
+//     elRefreshesDebug.innerText = `Refreshes: ${refreshes}`;
+//     if (position.coords.speed) {
+//         mySpeed = position.coords.speed;
+//     }
+//     myAccuracy = position.coords.accuracy;
+// }, geolocationError, { enableHighAccuracy: true, maximumAge: 1000 });
 
 let alpha: number = 0; // Tip north, + west
 let beta: number = 0; // Screen up, + tipped toward
@@ -107,16 +107,21 @@ function activateOrientation() {
             }
             if (ev.beta) {
                 beta = ev.beta;
+                while (beta < 0) {
+                    beta += 360;
+                }
+                beta -= 90;
+                beta = Math.min(-90, Math.max(beta, 30));
             }
         });
     }
 
-    if ("requestPermission" in DeviceOrientationEvent) {
-        (<any>DeviceOrientationEvent).requestPermission()
-            .then(listenOrientation);
-    } else {
-        listenOrientation();
-    }
+    // if ("requestPermission" in DeviceOrientationEvent) {
+    //     (<any>DeviceOrientationEvent).requestPermission()
+    //         .then(listenOrientation);
+    // } else {
+    //     listenOrientation();
+    // }
 }
 
 Object.assign(window, { activateOrientation });
@@ -128,8 +133,10 @@ let loopInstance = Date.now();
 let xDegreesPerMeter = 0;
 let yDegreesPerMeter = 0;
 
-const gridSpacing = 100;
-const gridlineLength = 10;
+const metersPerGridline = 10;
+const gridlineLength = 1;
+const mapWidth = 300;
+const pxPerMeter = gridlineLength / metersPerGridline;
 
 function draw() {
     const dt = (Date.now() - loopInstance) / 1000;
@@ -160,64 +167,185 @@ function draw() {
 
 let testStart = Date.now();
 
+const nearPlane = pxPerMeter;
+const xFov = Math.PI / 4;
+const yFov = xFov * elCanvas.height / elCanvas.width;
+const xFovLength = 2 * nearPlane * Math.tan(xFov / 2);
+const yFovLength = 2 * nearPlane * Math.tan(yFov / 2);
+let showPerspective = false;
+
 function grid() {
-    ctx.strokeStyle = "gray";
+    // Useful values.
 
-    const lngOffset = (renderCoords.lng % (xDegreesPerMeter * gridlineLength)) / (xDegreesPerMeter * gridlineLength) * gridSpacing;
-    const latOffset = (renderCoords.lat % (yDegreesPerMeter * gridlineLength)) / (yDegreesPerMeter * gridlineLength) * gridSpacing;
-    console.log(renderCoords.lng, xDegreesPerMeter, gridSpacing);
+    const lngOffset = (renderCoords.lat % xDegreesPerMeter) / xDegreesPerMeter * pxPerMeter;
+    const latOffset = (renderCoords.lat % yDegreesPerMeter) / xDegreesPerMeter * pxPerMeter;
 
-    const alphaRad = -alpha * Math.PI / 180;
-    const betaRad = Math.max(0, beta * Math.PI / 180);
+    const alphaRad = alpha * Math.PI / 180;
+    const betaRad = beta * Math.PI / 180;
 
-    const lineExtreme = 10 * Math.max(elCanvas.width, elCanvas.height);
+    const gridlineCount = 40;
 
-    function point([x, y]: [number, number]): [number, number] {
-        let x2 = x * Math.cos(alphaRad) - y * Math.sin(alphaRad);
-        let y2 = y * Math.cos(alphaRad) + x * Math.sin(alphaRad);
-        y2 *= Math.cos(betaRad);
+    const extreme = gridlineCount * gridlineLength;
 
-        return [x2, y2];
+    const z = (5 - 45 * Math.sin(betaRad) ** 1) * pxPerMeter;
+    const backup = 20 * pxPerMeter;
+
+    // Drawing functions.
+
+    function inFov(
+        [gridX, gridY]: [number, number],
+    ): [number, number, number] {
+        let x = gridX;
+        let y = gridY;
+
+        // Offset based on player position.
+        x += lngOffset;
+        y += latOffset;
+
+        // Rotate.
+        [x, y] = [
+            x * Math.cos(alphaRad) - y * Math.sin(alphaRad),
+            y = x * Math.sin(alphaRad) + y * Math.cos(alphaRad),
+        ];
+
+        // Determine location relative to center of frame of view.
+        let fovX = x; // Horizontal distance from the center of the frame of view.
+        let fovY = y * Math.cos(betaRad) - z * Math.sin(betaRad); // Outward distance from viewer.
+        let fovZ = -z * Math.cos(betaRad) - y * Math.sin(betaRad); // Height above viewer.
+
+        fovY += backup;
+
+        return [fovX, fovY, fovZ];
     }
 
-    ctx.beginPath();
+    function point(
+        [fovX, fovY, fovZ]: [number, number, number]
+    ): [number, number] {
+        // Project onto the near plane.
+        [fovX, fovZ] = [
+            fovX * nearPlane / fovY,
+            fovZ * nearPlane / fovY,
+        ];
 
-    for (let xFactor = -20; xFactor <= 20; xFactor++) {
-        let lineX = gridSpacing * xFactor - lngOffset;
+        // Expand to canvas.
+        let [x, y] = [
+            fovX / xFovLength * elCanvas.width,
+            fovZ / yFovLength * elCanvas.height,
+        ];
+        
+        // Flip Y.
+        y = -y;
 
-        let [startX, startY] = point([lineX, -lineExtreme]);
-        let [endX, endY] = point([lineX, lineExtreme]);
+        // Map coordinates to canvas.
+        x += elCanvas.width / 2;
+        y += elCanvas.height / 2;
 
-        startX += elCanvas.width / 2;
-        endX += elCanvas.width / 2;
-        startY += elCanvas.height / 2;
-        endY += elCanvas.height / 2;
+        return [x, y];
+    }
 
+    function line(
+        startGridX: number,
+        startGridY: number,
+        endGridX: number,
+        endGridY: number,
+        color: string,
+    ) {
+        let [startX, startY, startZ] = inFov([startGridX, startGridY]);
+        let [endX, endY, endZ] = inFov([endGridX, endGridY]);
+
+        if (startY < nearPlane && endY < nearPlane) {
+            return;
+        }
+
+        if (startY < nearPlane) {
+            const xDif = endX - startX;
+            const yDif = endY - startY;
+            const zDif = endZ - startZ;
+
+            const recoveryFactor = (yDif - endY + nearPlane) / yDif;
+
+            [startX, startY, startZ] = [
+                startX + xDif * recoveryFactor,
+                startY + yDif * recoveryFactor,
+                startZ + zDif * recoveryFactor,
+            ];
+        } else if (endY < nearPlane) {
+            const xDif = startX - endX;
+            const yDif = startY - endY;
+            const zDif = startZ - endZ;
+
+            const recoveryFactor = (yDif - startY + nearPlane) / yDif;
+
+            [endX, endY, endZ] = [
+                endX + xDif * recoveryFactor,
+                endY + yDif * recoveryFactor,
+                endZ + zDif * recoveryFactor,
+            ];
+        }
+
+        [startX, startY] = point([startX, startY, startZ]);
+        [endX, endY] = point([endX, endY, endZ]);
+
+        ctx.strokeStyle = color;
+
+        ctx.beginPath();
         ctx.moveTo(startX, startY);
         ctx.lineTo(endX, endY);
+        ctx.stroke();
     }
 
-    for (let yFactor = -20; yFactor <= 20; yFactor++) {
-        let lineY = gridSpacing * yFactor - latOffset;
+    // Grid lines.
 
-        let [startX, startY] = point([-lineExtreme, lineY]);
-        let [endX, endY] = point([lineExtreme, lineY]);
+    for (let y = -gridlineCount; y <= gridlineCount; y++) {
+        const shade = Math.min(255, (gridlineCount - Math.abs(y)) / gridlineCount * 255);
+        const color = `rgb(${shade}, ${shade}, ${shade})`;
 
-        startX += elCanvas.width / 2;
-        endX += elCanvas.width / 2;
-        startY += elCanvas.height / 2;
-        endY += elCanvas.height / 2;
+        const startGridX = -extreme;
+        const startGridY = y * gridlineLength;
+        const endGridX = extreme;
+        const endGridY = y * gridlineLength;
 
-        ctx.moveTo(startX, startY);
-        ctx.lineTo(endX, endY);
+        line(startGridX, startGridY, endGridX, endGridY, color );
     }
 
-    ctx.stroke();
+    for (let x = -gridlineCount; x <= gridlineCount; x++) {
+        const shade = Math.min(255, (gridlineCount - Math.abs(x)) / gridlineCount * 255);
+        const color = `rgb(${shade}, ${shade}, ${shade})`;
 
-    ctx.strokeStyle = "red";
+        const startGridX = x * gridlineLength;
+        const startGridY = -extreme;
+        const endGridX = x * gridlineLength;
+        const endGridY = extreme;
+
+        line(startGridX, startGridY, endGridX, endGridY, color);
+    }
+
+    // Player dot.
+
+    ctx.fillStyle = "red";
     ctx.beginPath();
-    ctx.arc(elCanvas.width / 2, elCanvas.height / 2, 10, 0, 2 * Math.PI);
-    ctx.stroke();
+
+    const playerFovLoc = inFov([0, 0]);
+    if (playerFovLoc[1] > 0) {
+        const [x, y] = point(playerFovLoc);
+
+        ctx.arc(x, y, 8, 0, 2 * Math.PI);
+        ctx.fill();
+    }
 }
 
 draw();
+
+document.addEventListener("keydown", ev => {
+    if (ev.key == "ArrowDown") {
+        beta -= 2;
+    } else if (ev.key == "ArrowUp") {
+        beta += 2;
+    } else if (ev.key == "ArrowLeft") {
+        alpha -= 2;
+    } else if (ev.key == "ArrowRight") {
+        alpha += 2;
+    } else if (ev.key == " ") {
+        showPerspective = !showPerspective;
+    }
+});
